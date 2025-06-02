119f74d6bbda3abfe3ae88c4919a80b07d684168
ahr <ahrasheed@gmail.com>
2025-06-02 19:22:27 +0200

    (fix) Fixes input error to Whisper models

--- src/roboml/models/speech_to_text.py
+++ src/roboml/models/speech_to_text.py
@@ -1,4 +1,6 @@
+import base64
 import numpy as np
+
 from faster_whisper import WhisperModel
 
 from roboml.interfaces import SpeechToTextInput
@@ -14,16 +16,15 @@ class Whisper(ModelTemplate):
     def _initialize(
         self,
         checkpoint: str = "small.en",
-        quantization: str = "int8",
+        compute_type: str = "int8",
     ) -> None:
         """
         Initializes the model.
         """
-        # Run on GPU with FP16
         self.model = WhisperModel(
             checkpoint,
             device=self.device,
-            compute_type=quantization,
+            compute_type=compute_type,
         )
 
     def _inference(self, data: SpeechToTextInput) -> dict:
@@ -34,8 +35,15 @@ class Whisper(ModelTemplate):
         :returns:   Model output
         :rtype:     dict
         """
+        # Treat strings as base64 encoded numpy array
+        audio = (
+            np.frombuffer(base64.b64decode(data.query), dtype=np.float32)
+            if isinstance(data.query, str)
+            else data.query
+        )
+
         # make inference
         segments, _ = self.model.transcribe(
-            audio=data.query, max_new_tokens=data.max_new_tokens
+            audio=audio, max_new_tokens=data.max_new_tokens
         )
         return {"output": " ".join([s.text.strip() for s in segments])}
